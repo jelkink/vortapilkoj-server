c4b8af56391bf3e3a970e48246eec79f474bb1ad
Jos Elkink <jelkink@gmail.com>
2024-12-27 17:53:00 +0000

    Add separate file for REST interface

---
+++ src/public/rest/index.php
@@ -0,0 +1,44 @@
+<?php
+// during development only!
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$path_to_library = "../library/";
+
+require_once($path_to_library . "class.database.php");
+require_once($path_to_library . "class.wordlist.php");
+
+$db = new Database();
+$db->Connect();
+
+$wl = new WordList($db);
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['command']) && $_GET['command'] === 'LIST_NAMES') {
+  $response = [
+    'status' => 'success',
+    'message' => $wl->get_all_list_names()
+  ];
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['command']) && $_GET['command'] === 'LIST') {
+  if (isset($_GET['name'])) {
+    $listName = $_GET['name'];
+    $wl->read_list($listName);
+    $response = [
+      'status' => 'success',
+      'message' => $wl->get_list()
+    ];
+  } else {
+    $response = [
+      'status' => 'error',
+      'message' => 'List name not provided'
+    ];
+  }
+} else {
+  $response = [
+    'status' => 'error',
+    'message' => 'Command not found'
+  ];
+}
+
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
