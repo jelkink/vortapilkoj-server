29f8f414b90a6bb55dfdf5f9030b2a292b319f77
Jos Elkink <jelkink@gmail.com>
2024-12-28 16:51:45 +0000

    Add intro text

--- src/library/page.home.php
+++ src/library/page.home.php
@@ -1,5 +1,3 @@
 <h2>Hejmo</h2>
 
-<?php
-
-?>
+<p>Bonvenon al la administra paĝo de Vortapilkoj. Vi povas uzi ĉi tiun retejon por aldoni, redakti aŭ forigi, kaj importi aŭ eksporti, vortlistojn por la Android-aplikaĵo Vortapilkoj.</p>
