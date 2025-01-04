40c16affb6c006dc35fc99f8abc4f930a49367ed
Jos Elkink <jelkink@gmail.com>
2025-01-04 11:53:28 +0000

    Minor tweaks to home page text.

--- src/library/page.home.php
+++ src/library/page.home.php
@@ -1,9 +1,11 @@
 <h2>Hejmo</h2>
 
-<p>Bonvenon al la administra paĝo de Vortapilkoj. Vi povas uzi ĉi tiun retejon por aldoni, redakti aŭ forigi, kaj importi aŭ eksporti, vortlistojn por la Android-aplikaĵo Vortapilkoj.</p>
+<p>Bonvenon al la administra paĝo de Vortapilkoj.</p>
+
+<p>Vi povas uzi ĉi tiun retejon por aldoni, redakti aŭ forigi, kaj importi aŭ eksporti, vortlistojn por la Android-aplikaĵo Vortapilkoj.</p>
 
 <?php if ($session->is_logged_in()): ?>
-  <p>Vi estas ensalutinta. <a href="index.php?page=logout&session=<?php echo $sessionid ?>">Elsaluti</a></p>
+  <p>Vi estas ensalutinta.</p>
 <?php else: ?>
 <form action="index.php?page=login" method="post">
   <label for="email">Retpoŝto:</label>
